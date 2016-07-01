4cbb0a0be010281dcab131c4ac770b8015f72b62
pachamaltese <mauriciovargas@ug.uchile.cl>
2016-07-01 18:40:15 -0300

    updated index

--- gh-pages/_makeindex.sh
+++ gh-pages/_makeindex.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-ls | egrep '(html|json)' |
+ls | egrep '(html)' |
 perl -e 'print "<html><body><ul>"; while(<>) { chop $_; print "<li><a href=\"./$_\">$_</a></li>";} print "</ul></body></html>"' > index.html
