64c057decff1798e15555b607ec273edeb0ff2cc
pachamaltese <mauriciovargas@ug.uchile.cl>
2016-07-01 00:33:34 -0300

    csv download enabled

--- gh-pages/encuentros_idh.js
+++ gh-pages/encuentros_idh.js
@@ -1,32 +1,40 @@
-          var visualization = d3plus.viz()
-          .container("#encuentros_idh")
-          .data("encuentros_idh.json")
-          .type("scatter")
-          .width(false)
-          .height(false)
-          .resize(true)
-          .id(["comuna"])
-          .size("pob14_comuna")
-          .x("encuentros_poblacion")
-          .y("idh")
-          .format({
-            "text": function(text, params) {
-              if (text === "pob14_comuna") {
-                return "Población de la comuna";
-              }
-              if (text === "encuentros_poblacion") {
-                return "Encuentros locales por cada 10,000 habitantes mayores de 14 años";
-              }
-              if (text === "idh") {
-                return "Índice de Desarrollo Humano"
-              }
-              else {
-                return d3plus.string.title(text, params);
-              }
-            }  
-          })
-          .font({"family": "Lato"})
-          .title("Encuentros locales versus Índice de Desarrollo Humano")
-          .title({"sub": "Comunas con más de 10,000 habitantes mayores de 14 años"})
-          .tooltip(["pob14_comuna","idh","encuentros_poblacion"])
-          .draw()
+var visualization = d3plus.viz()
+.container("#encuentros_idh")
+.data("encuentros_idh.json")
+.type("scatter")
+.width(false)
+.height(false)
+.resize(true)
+.id(["comuna"])
+.size("pob14_comuna")
+.x("encuentros_poblacion")
+.y("idh")
+.format({
+  "text": function(text, params) {
+    if (text === "pob14_comuna") {
+      return "Población de la comuna";
+    }
+    if (text === "encuentros_poblacion") {
+      return "Encuentros locales por cada 10,000 habitantes mayores de 14 años";
+    }
+    if (text === "idh") {
+      return "Índice de Desarrollo Humano"
+    }
+    else {
+      return d3plus.string.title(text, params);
+    }
+  }  
+})
+.font({"family": "Lato"})
+.title("Encuentros locales versus Índice de Desarrollo Humano")
+.title({"sub": "Comunas con más de 10,000 habitantes mayores de 14 años"})
+.tooltip(["pob14_comuna","idh","encuentros_poblacion"])
+.ui([
+  {
+    "method": function(){
+      visualization.csv(); // passing no values will download data as csv file
+    },
+    "value": ["Download as CSV"]
+  }
+])
+.draw()
--- gh-pages/encuentros_participacion.js
+++ gh-pages/encuentros_participacion.js
@@ -1,40 +1,48 @@
-          var visualization = d3plus.viz()
-          .container("#encuentros_participacion")
-          .data("encuentros_participacion.json")
-          .type("scatter")
-          .width(false)
-          .height(false)
-          .resize(true)
-          .id(["comuna"])
-          .size("pob14_comuna")
-          .x("encuentros_poblacion")
-          .y("indice_participacion")
-          .format({
-            "text": function(text, params) {
-              if (text === "pob14_comuna") {
-                return "Población de la comuna";
-              }
-              if (text === "encuentros_poblacion") {
-                return "Encuentros locales por cada 10,000 habitantes mayores de 14 años";
-              }
-              if (text === "indice_participacion") {
-                return "Participación";
-              }
-              else {
-                return d3plus.string.title(text, params);
-              }
-            },
-            "number": function(number, params) {
-              var formatted = d3plus.number.format(number, params);
-              if (params.key === "indice_participacion") {
-                return formatted + "%";
-              }
-              else {
-                return formatted;
-              }
-            }  
-          })
-          .font({"family": "Lato"})
-          .title("Encuentros locales versus participación en elecciones en Santiago")
-          .tooltip(["pob14_comuna","indice_participacion","encuentros_poblacion"])
-          .draw()
+var visualization = d3plus.viz()
+.container("#encuentros_participacion")
+.data("encuentros_participacion.json")
+.type("scatter")
+.width(false)
+.height(false)
+.resize(true)
+.id(["comuna"])
+.size("pob14_comuna")
+.x("encuentros_poblacion")
+.y("indice_participacion")
+.format({
+  "text": function(text, params) {
+    if (text === "pob14_comuna") {
+      return "Población de la comuna";
+    }
+    if (text === "encuentros_poblacion") {
+      return "Encuentros locales por cada 10,000 habitantes mayores de 14 años";
+    }
+    if (text === "indice_participacion") {
+      return "Participación";
+    }
+    else {
+      return d3plus.string.title(text, params);
+    }
+  },
+  "number": function(number, params) {
+    var formatted = d3plus.number.format(number, params);
+    if (params.key === "indice_participacion") {
+      return formatted + "%";
+    }
+    else {
+      return formatted;
+    }
+  }  
+})
+.font({"family": "Lato"})
+.title("Encuentros locales versus participación en elecciones en Santiago")
+.tooltip(["pob14_comuna","indice_participacion","encuentros_poblacion"])
+.ui([
+      {
+        "method": function(){
+          visualization.csv(); // passing no values will download data as csv file
+        },
+        "value": ["Download as CSV"]
+      }
+])
+.draw()
